62304193da57b08f257c3a08789f0d7522896591
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:41 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/test/java/org/apache/shiro/spring/boot/oltu/authz/OltuOAuth2AuthenticationFilterTest.java
+++ src/test/java/org/apache/shiro/spring/boot/oltu/authz/OltuOAuth2AuthenticationFilterTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link OltuOAuth2AuthenticationFilter }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("OltuOAuth2AuthenticationFilter Tests")
